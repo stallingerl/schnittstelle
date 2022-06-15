6f9c54d8f69cf5902bcc6af2b0238737dfe01421
stallingerl <lena.stallinger@webanizer.de>
2022-06-15 15:03:39 +0200

    mit SSL Server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,3 @@
-import { createRequire } from "module"; // Bring in the ability to create the 'require' method
-const require = createRequire(import.meta.url); // construct the require method
 import 'dotenv/config'
 import { connect } from "./config/database.js"
 connect()
