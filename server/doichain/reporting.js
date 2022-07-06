84f4a5970ee21c253827c267c9ec698ec28a8bfe
stallingerl <lena.stallinger@webanizer.de>
2022-07-06 09:17:18 +0000

    reporting

--- server/doichain/reporting.js
+++ server/doichain/reporting.js
@@ -9,17 +9,31 @@ export async function sendReport(foundMatchingMeterdata) {
         await getToken()
     }
 
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${s.token}`,
+    const config = {
+        headers: { 'Authorization': `Bearer ${s.token}` }
     };
 
-    resStatus = await postReport(foundMatchingMeterdata, headers)
+    const bodyParameters =             {
+        "booking_id": "5ce6682e3147b5fa02cf3efd",
+        "mfa_id": "5ce6682e3147b5fa02cf3efe",
+        "etus": [
+            {
+                "date": "2019-04-03T18:00:00",
+                "energy": 42.7
+            },
+            {
+                "date": "2019-04-03T18:15:00",
+                "energy": 85.7
+            }
+        ]
+    }
+
+    resStatus = await postReport(foundMatchingMeterdata, bodyParameters, config)
 
     // If last token was expired, get new one and try again
     if (resStatus == 401) {
         await getToken()
-        resStatus = await postReport(foundMatchingMeterdata, headers)
+        resStatus = await postReport(foundMatchingMeterdata,  bodyParameters, config)
     }
 
     if (resStatus == 200) {
@@ -30,25 +44,12 @@ export async function sendReport(foundMatchingMeterdata) {
 
 }
 
-async function postReport(foundMatchingMeterdata, headers) {
+async function postReport(foundMatchingMeterdata, bodyParameters, config) {
     let resStatus
     try {
         await axios.post("https://backend.consolinno-test.nemospot.de/reports",
-            {
-                "booking_id": "5ce6682e3147b5fa02cf3efd",
-                "mfa_id": "5ce6682e3147b5fa02cf3efe",
-                "etus": [
-                    {
-                        "date": "2019-04-03T18:00:00",
-                        "energy": 42.7
-                    },
-                    {
-                        "date": "2019-04-03T18:15:00",
-                        "energy": 85.7
-                    }
-                ]
-            },
-            { headers }
+            bodyParameters,
+            { headers: {"Authorization" : `Bearer ${s.token}`} }
         )
             .then(res => {
                 if (res.status == 200) {
